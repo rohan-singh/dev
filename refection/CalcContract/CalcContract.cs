af4e23df71208f5733533acf6b226357dcf36af9
Rohan Singh <rohan.vishen@gmail.com>
2017-08-29 19:49:33 +0530

    Create CalcContract.cs

---
+++ refection/CalcContract/CalcContract.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcContract
+{
+    public interface ICalculator
+    {
+        int Sum(int a, int b);
+
+    }
+}
